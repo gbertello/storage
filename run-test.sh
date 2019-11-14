8f5820d35e878421c0b181d07b08521a3f3d8c2d
Guillaume BERTELLO <guillaume.bertello@amadeus.com>
2019-11-14 21:50:30 +0100

    Simplify run test

--- run-test.sh
+++ run-test.sh
@@ -9,7 +9,5 @@ source ~/.profile
 docker network create --driver bridge test &> /dev/null || true
 docker-compose -f docker-compose-test.yml down || true
 
-mkdir -p storage/db/test
-
 docker-compose -f docker-compose-test.yml build
 docker-compose -f docker-compose-test.yml up -d
