789ea9d54099b9ecc7a141e9e74297caba652c4e
Guillaume BERTELLO <guillaume.bertello@amadeus.com>
2019-11-14 21:26:32 +0100

    Fix run test

--- run-test.sh
+++ run-test.sh
@@ -9,5 +9,7 @@ source ~/.profile
 docker network create --driver bridge test &> /dev/null || true
 docker-compose -f docker-compose-test.yml down || true
 
+mkdir -p storage/db/test
+
 docker-compose -f docker-compose-test.yml build
 docker-compose -f docker-compose-test.yml up -d
